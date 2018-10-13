9aed4f356c9c1bd98cd77bf4113bf1fa0836f9d4
i-ivanova <inna.ivanova.vr@gmail.com>
2018-10-13 23:45:05 +0100

    Update text_game.py
    Newest Version

--- text_game.py
+++ text_game.py
@@ -1,11 +1,13 @@
 import pygame
 import pygame_textinput
 import pygame_button
+import round_rect as rr
 import os
 
 DECISIONS = {"username": ""}
 pygame.init()
 SCREEN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen_w, screen_h = SCREEN.get_width(), SCREEN.get_height()
 pygame.display.set_caption('The Escape Game')
 
 def load_image(path):
@@ -16,8 +18,6 @@ class Kayla(pygame.sprite.Sprite):
 	def __init__(self):
 		super(Kayla, self).__init__()
 		kayla_images_dance = ["kayla{}.png".format(str(x)) for x in range(1,7)]
-		print (kayla_images_dance)
-
 		self.index = 0
 		self.images = []
 		for name in kayla_images_dance:
@@ -26,7 +26,7 @@ class Kayla(pygame.sprite.Sprite):
 		self.image = self.images[self.index]
 		self.normal = False
 		self.rect = self.image.get_rect()
-		self.rect.center = (SCREEN.get_width() / 2, SCREEN.get_height() / 2)
+		self.rect.center = (SCREEN.get_width() / 2, SCREEN.get_height() / 7 * 4)
 
 	def update(self):
 		self.index += 1
@@ -44,6 +44,35 @@ class Background(pygame.sprite.Sprite):
 		self.image = pygame.transform.scale(self.image, (SCREEN.get_width(), SCREEN.get_height()))
 		self.rect = self.image.get_rect()
 		self.rect.left, self.rect.top = location
+
+class Crowd(pygame.sprite.Sprite):
+	def __init__(self, image_file, location):
+		super(Crowd, self).__init__()
+		self.image = load_image(image_file)
+		#self.image = pygame.transform.scale(self.image, (SCREEN.get_width() // 3, SCREEN.get_height() // 3))
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = location
+
+class DecisionMakingBoxes(pygame.sprite.Sprite):
+	def __init__(self, color, captions):
+		super(DecisionMakingBoxes, self).__init__()
+		font = pygame.font.SysFont('tlwgtypo', 50)
+
+		rect1 = (screen_w / 10 * 1, screen_h / 19 * 15,  screen_w / 10 * 4, screen_h / 12 * 1)
+		rect2 = (screen_w / 15 * 8, screen_h / 19 * 15,  screen_w / 10 * 4, screen_h / 12 * 1)
+		rect3 = (screen_w / 10 * 1, screen_h / 17 * 15,  screen_w / 10 * 4, screen_h / 12 * 1)
+		rect4 = (screen_w / 15 * 8, screen_h / 17 * 15,  screen_w / 10 * 4, screen_h / 12 * 1)
+
+		self.decision1 = pygame_button.PygButton(rect=rect1, caption=captions[1], font=font, bgcolor=color)
+		self.decision2 = pygame_button.PygButton(rect=rect2, caption=captions[2], font=font, bgcolor=color)
+		self.decision3 = pygame_button.PygButton(rect=rect3, caption=captions[3], font=font, bgcolor=color)
+		self.decision4 = pygame_button.PygButton(rect=rect4, caption=captions[4], font=font, bgcolor=color)
+
+	def draw(self, screen):
+		self.decision1.draw(screen)
+		self.decision2.draw(screen)
+		self.decision3.draw(screen)
+		self.decision4.draw(screen)
 
 
 def setup_front_page(pygame_screen):
@@ -101,7 +130,8 @@ def front_page():
 			if 'click' in exit_button.handleEvent(event):
 				return
 
-		# constatly update
+			# constatly update
+
 		username_input.update(events)
 		SCREEN.blit(username_input.get_surface(), (username_x , username_y - 70))
 		play_button.draw(SCREEN)
@@ -117,10 +147,26 @@ def start_game(username_input):
 	DECISIONS["username"] = username_input.get_text()
 	SCREEN.fill((18, 17, 44))
 	pygame.display.update()
+
 	kayla_dance = Kayla()
 	all_sprites = pygame.sprite.Group()
-	all_sprites.add(kayla_dance)
 	BackGround = Background(os.path.join("Crowd", "dance_floor.png"), [0,0])
+	crowds = []
+	crowds.append(Crowd(os.path.join("Crowd", "crowd_dance.png"), [screen_w / 3, screen_h / 3]))
+
+	crowds.append(Crowd(os.path.join("Crowd", "crowd_dance.png"), [screen_w / 11*2, screen_h / 10*4]))
+	crowds.append(Crowd(os.path.join("Crowd", "crowd_dance.png"), [screen_w / 11*6, screen_h / 10*4]))
+
+	crowds.append(Crowd(os.path.join("Crowd", "crowd_dance.png"), [screen_w / 9, screen_h / 10*5]))
+	crowds.append(Crowd(os.path.join("Crowd", "crowd_dance.png"), [screen_w / 9*6, screen_h / 10*5]))
+
+	for crowd in crowds:
+		all_sprites.add(crowd)
+	all_sprites.add(kayla_dance)
+
+	test = {1: "Test", 2: "Text", 3: "For", 4: "Fun"}
+	decisions = DecisionMakingBoxes((129, 123, 134), test)
+
 
 	while True:
 		pygame.time.delay(150)
@@ -130,10 +176,23 @@ def start_game(username_input):
 			if keys[pygame.K_ESCAPE]:
 				return
 
+			if 'click' in decisions.decision1.handleEvent(event):
+				return
+
+			if 'click' in decisions.decision2.handleEvent(event):
+				return
+
+			if 'click' in decisions.decision3.handleEvent(event):
+				return
+
+			if 'click' in decisions.decision4.handleEvent(event):
+				return
+
 		SCREEN.fill([255, 255, 255])
 		SCREEN.blit(BackGround.image, BackGround.rect)
 		kayla_dance.update()
 		all_sprites.draw(SCREEN)
+		decisions.draw(SCREEN)
 		pygame.display.update()
 
 
